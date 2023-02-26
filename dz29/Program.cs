e1f62a42ae2883a3303f71d51c3c3b47437aa847
AndreyMir <miroshal@mail.ru>
2023-02-27 02:04:52 +0300

    Задача 29

---
+++ dz29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void NumArray(int[] number)
+{
+    int lenght = number.Length;
+    int index = 0;
+    while(index < lenght)
+    {
+        number[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] numb)
+{
+    int count = numb.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write(numb[position]);
+        position++;
+    }
+}
+int[] array = new int[8];
+NumArray(array);
+PrintArray(array);
