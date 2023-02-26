55699551b4abd99e5a26ef091802b0845b53b95c
AndreyMir <miroshal@mail.ru>
2023-02-27 01:24:19 +0300

    Задача 25

---
+++ dz25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+System.Console.Write("Введите число A: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите число B: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int Exponentiation(int BasisOfDigree, int DegreeIndicator)
+{
+    int res = 1;
+    for (int i = 0; i < DegreeIndicator; i++)
+    {
+        res = res * BasisOfDigree;
+    }
+    return res;
+}
+System.Console.WriteLine(Exponentiation(num1, num2));
